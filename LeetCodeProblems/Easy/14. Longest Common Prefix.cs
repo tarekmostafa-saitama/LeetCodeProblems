587b40fa45f33b8c87c52782f268a55f79d2abd9
Tarek Mostafa <tarek.r.mostafa@gmail.com>
2025-08-11 03:49:28 +0300

    Add LongestCommonPrefixString class for prefix finding
    This commit introduces the `LongestCommonPrefixString` class in the `LeetCodeProblems.Easy` namespace. It includes the `LongestCommonPrefix` method, which computes the longest common prefix from an array of strings. The method handles empty arrays, identifies the shortest string, and checks character matches across all strings to build the prefix.

---
+++ LeetCodeProblems/Easy/14. Longest Common Prefix.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeProblems.Easy;
+
+public class LongestCommonPrefixString {
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs.Length == 0) return "";
+        string shortedString = strs.MinBy(x => x.Length);
+        string prefix = "";
+        for (int i = 0; i < shortedString.Length; i++)
+        {
+            bool allMatch = strs.All(x => x[i] == shortedString[i]);
+            if (allMatch)
+            {
+                prefix += shortedString[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+        return prefix;
+    }
+}
