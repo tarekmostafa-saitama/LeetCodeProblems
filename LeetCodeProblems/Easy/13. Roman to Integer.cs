2cc609c503fd848bec40540817ed41de3eec7601
Tarek Mostafa <tarek.r.mostafa@gmail.com>
2025-08-11 03:36:34 +0300

    Add RomanToInteger class for numeral conversion
    Implement a new `RomanToInteger` class in the `LeetCodeProblems.Easy` namespace. The class features a `RomanToInt` method that converts Roman numeral strings to their integer values using a dictionary for mapping and a loop to handle numeral addition and subtraction rules.

---
+++ LeetCodeProblems/Easy/13. Roman to Integer.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeProblems.Easy;
+
+public class RomanToInteger {
+    public int RomanToInt(string s)
+    {
+        // Create a dictionary to map Roman numerals to their integer values
+        Dictionary<char, int> romanMap = new Dictionary<char, int>()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+        int result = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i + 1 < s.Length && romanMap[s[i]] < romanMap[s[i + 1]])
+            {
+                result -= romanMap[s[i]];
+            }
+            else
+            {
+                result += romanMap[s[i]];
+            }
+        }
+
+        return result;
+    }
+}
