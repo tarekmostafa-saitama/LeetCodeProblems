62651e908f3f5fc2ed471c511a838f300af47842
Tarek Mostafa <tarek.r.mostafa@gmail.com>
2025-08-11 04:15:08 +0300

    Add ValidParentheses class with IsValid method
    Introduces a new class `ValidParentheses` in the namespace `LeetCodeProblems.Easy`. The `IsValid` method checks if a string of parentheses is valid using a stack and a dictionary for mapping. It iterates through the string, ensuring that all opening parentheses are properly matched with their corresponding closing ones.

---
+++ LeetCodeProblems/Easy/20. Valid Parentheses.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeProblems.Easy;
+
+public class ValidParentheses {
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        Dictionary<char, char> parenthesesMap =
+            new Dictionary<char, char>() { { '(', ')' }, { '{', '}' }, { '[', ']' } };
+
+        foreach (char c in s)
+        {
+            if (parenthesesMap.ContainsKey(c))
+            {
+                stack.Push(c);
+            }
+            else if (parenthesesMap.ContainsValue(c))
+            {
+                if (stack.Count == 0 || parenthesesMap[stack.Pop()] != c)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
